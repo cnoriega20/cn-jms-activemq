0682b13d96b5bc2bef38792a90d52f8576e58abf
cnoriega20 <c79.noriega@gmail.com>
2021-02-23 23:26:09 -0600

    Springboot Initialzr project structure.

---
+++ src/main/java/com/traning/jms/activemq/CnJmsActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.traning.jms.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnJmsActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnJmsActivemqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/traning/jms/activemq/CnJmsActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traning.jms.activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnJmsActivemqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
