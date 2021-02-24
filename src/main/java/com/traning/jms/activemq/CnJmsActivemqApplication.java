5b5f76cec667578b06f8d36fda8919dace537207
cnoriega20 <c79.noriega@gmail.com>
2021-02-24 00:58:57 -0600

    Send and receive JMS messages

--- src/main/java/com/traning/jms/activemq/CnJmsActivemqApplication.java
+++ src/main/java/com/traning/jms/activemq/CnJmsActivemqApplication.java
@@ -1,13 +1,53 @@
 package com.traning.jms.activemq;
 
+import com.traning.jms.activemq.model.Email;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
 
 @SpringBootApplication
+@EnableJms
+@Slf4j
 public class CnJmsActivemqApplication {
 
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                             DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
+
+
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter jacksonJmsMessageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+    // Launch the application
     public static void main(String[] args) {
-        SpringApplication.run(CnJmsActivemqApplication.class, args);
+        ConfigurableApplicationContext context =  SpringApplication.run(CnJmsActivemqApplication.class, args);
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+        //Send a message with a POJO
+        log.info("Sending an email message");
+        jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "hey you!!!"));
+
     }
 
 }
